93ea89d8d53f5e9f619690e1ff5123b612da647d
tsuruoka <tryusuke0809@gmail.com>
2023-09-10 17:12:19 +0900

    OGPタグを追加する

--- src/routes/products/[id]/+page.svelte
+++ src/routes/products/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { page } from '$app/stores';
   import { afterNavigate } from '$app/navigation';
   import Slider from './Slider.svelte';
   
@@ -13,6 +14,14 @@
     userRequest = fetch(`/api/self`).then((res) => res.json());
   })
 </script>
+
+<svelte:head>
+  <meta name="twitter:card" content="summary" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={$page.url} />
+  <meta property="og:title" content={product.name} />
+  <meta property="og:description" content={`${product.name} - ${product.price}円`} />
+</svelte:head>
 
 <header class="header">
   <a class="header-title" href="/">Svelte EC</a>
